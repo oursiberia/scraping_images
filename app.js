6af04a22217adc0d1df13eba65a837a15c3c276f
Ana <agiraldow@gmail.com>
2014-04-16 18:36:16 -0400

    added some filters for input

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var http = require('http');
-var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cheerio = require('cheerio');
 var request = require('request');
+var path = require('path');
 var url = require('url');
-var normalizeUrl = require('normalizeurl');
 
 var routes = require('./routes');
 
@@ -25,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 app.get('/', function(req, res){
-    res.render('index', { title: 'scraping stuff', images: [] })
+  res.render('index', { title: 'scraping stuff', images: [] })
 });
 
 app.get('/scrape-images', function(req, res){
@@ -33,23 +32,28 @@ app.get('/scrape-images', function(req, res){
   request(reqUrl, function (error, response, html){
     if (!error && response.statusCode == 200){
       var $ = cheerio.load(html);
-      var images = [];
+      var images = {};
       $('img').each(function(i, element){
         var src = element.attribs.src;
-        var image;
-        if (src.indexOf('http') == -1
+        var alt = element.attribs.alt;
+        // var isImage = (/\.(gif|jpg|jpeg|tiff|png)$/i);
+        // && isImage.test(src);
+        if (src) {
+          var image;
+          if (src.indexOf('http') == -1
             && src.indexOf('https') == -1
-            && src.indexOf('www') == -1
+            && src.indexOf('www.') == -1
             && src.indexOf('.org') == -1
             && src.indexOf('.com') == -1
             && src.indexOf('.net') == -1
             && src.indexOf('.io') == -1)
-        {
-          image = normalizeUrl(reqUrl + src);
-        } else {
-          image = src;
+          {
+            image = { url: url.resolve(reqUrl, src), alt: alt };
+          } else {
+            image = { url: src, alt: alt };
+          }
+          images[i] = image;
         }
-        images.push(image);
       });
       res.render('index', { title: 'scraping stuff', url: reqUrl, images: images })
     }
@@ -58,9 +62,9 @@ app.get('/scrape-images', function(req, res){
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 /// error handlers
@@ -68,21 +72,21 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
