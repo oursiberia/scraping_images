4eca9f3a1cd363eb1ffdadce42397d5f216de0db
Ana <agiraldow@gmail.com>
2014-04-24 00:19:49 -0400

    fixed instructions hiding/showing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	console.log('mainjs here');
 
+	$('.instructions').show();
+
 	$('.item').each(function(i, element){
 		var me = this;
 		var bg = $(me).css('background-image');
@@ -10,15 +12,15 @@ $(document).ready(function(){
 		image.onload = function(){
 			if (this.width * this.height > 40000){
 				$(me).parent().show();
+				$('.instructions').hide();
 				$(me).attr('data-dimensions', this.width + ' x ' + this.height);
 			} else {
 				$(me).parent().remove();
 			}
-			if ($('.results').is(':empty')){
-				$('.instructions').show();
-			}
+			// if ($('.results').is(':empty')){
+			// 	$('.instructions').show();
+			// }
 		};
 	});
 
-	
 });
